0c2a6e4e7d2a6bc66320689a3a3387cf430fac9f
Konstantim <const.trofimov@gmail.com>
2021-01-20 00:30:05 +0300

    created

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+	<section>
+		<h1>About page</h1>
+
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, nulla!</p>
+
+		<h2>Lorem, ipsum dolor.</h2>
+
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt reiciendis facere cum cupiditate doloremque soluta amet debitis, a, nemo voluptate consequatur quos? Eos, esse?</p>
+
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, sunt mollitia. Illo ratione cupiditate repellendus dolores. Eum fugiat perspiciatis amet non soluta tenetur qui libero consequuntur tempora deleniti reprehenderit eius reiciendis ut provident suscipit quia, cumque tempore necessitatibus recusandae ullam aut.</p>
+
+		<p>Lorem ipsum dolor sit amet consectetur.</p>
+	</section>
+
+	
+</template>
+
+<script>
+	export default {
+		middleware: ['auth']
+	}
+</script>
